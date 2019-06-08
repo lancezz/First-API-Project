a1b699034b0a94f53455469d058416fbb4ce10a9
Cloud <qwerty0726@hotmail.com>
2019-06-08 10:37:06 -0700

    Work

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// API key: https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=3GTYGsn0IxOVh2JyqLtbyXN8vuHcZxwVwEvB7Fk5&location=Denver+CO
+
+function processInput(input) {
+    console.log(food);
+}
